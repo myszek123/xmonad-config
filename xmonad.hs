7e540fac917db02550d379271102b767404d6066
Jakub Myszka <Jakub Myszka>
2016-05-10 18:23:51 +0200

    some freestyle changes
    works with 0.12
    dzen2 added
    some comments removed
    still heavy WIP

--- xmonad.hs
+++ xmonad.hs
@@ -2,14 +2,20 @@
 -- Author: Vic Fryzel
 -- http://github.com/vicfryzel/xmonad-config
  
+-- LANGUAGE DeriveDataTypeable, NoMonomorphismRestriction, TypeSynonymInstances, MultiParamTypeClasses #-}
+-- LANGUAGE DeriveDataTypeable, NoMonomorphismRestriction, MultiParamTypeClasses, ImplicitParams #-}
+
 import System.IO
 import System.Exit
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageDocks (ToggleStruts(ToggleStruts))
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.FadeInactive
 
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spiral
@@ -20,36 +26,37 @@ import XMonad.Layout.Magnifier
 import XMonad.Layout.Grid
 import XMonad.Layout.PerWorkspace
 
+import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import qualified XMonad.Util.Themes as Theme
 
 import XMonad.Prompt
-import XMonad.Prompt.AppendFile
 import XMonad.Prompt.Shell
+import XMonad.Prompt.AppendFile
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 import Data.List (isInfixOf, isPrefixOf)
 
-import qualified XMonad.Actions.FlexibleManipulate as Flex
 import XMonad.Actions.UpdatePointer
 import XMonad.Actions.RotSlaves
+import qualified XMonad.Actions.FlexibleManipulate as Flex
 
 import XMonad.ManageHook
-import XMonad.Util.NamedScratchpad
+
 
 scratchpads = [
-     NS "term" "gnome-terminal --role myterm" (role =? "myterm")
+     NS "term" "mate-terminal --role myterm" (role =? "myterm")
          (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3)),
-     NS "notes" "gnome-terminal --role mynotes -x vim ~/notes/notes.txt" (role =? "mynotes")
+     NS "notes" "mate-terminal --role mynotes -x vim ~/notes/notes.txt" (role =? "mynotes")
          (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3)),
-     NS "mytodo" "gnome-terminal --role mytodo -x vim ~/notes/home/todo.txt" (role =? "mytodo")
+     NS "mytodo" "mate-terminal --role mytodo -x vim ~/notes/home/todo.txt" (role =? "mytodo")
          (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3)),
-     NS "worktodo" "gnome-terminal --role worktodo -x vim ~/notes/work/todo.txt" (role =? "worktodo")
+     NS "worktodo" "mate-terminal --role worktodo -x vim ~/notes/work/todo.txt" (role =? "worktodo")
          (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3)),
-     NS "htop" "gnome-terminal --role myhtop -x htop" (role =? "myhtop") defaultFloating
+     NS "htop" "mate-terminal --role myhtop -x htop" (role =? "myhtop") defaultFloating
  ] where role = stringProperty "WM_WINDOW_ROLE"
 
 myTabTheme = (Theme.theme Theme.xmonadTheme)
@@ -94,7 +101,7 @@ myNumlockMask   = mod2Mask
 --
 -- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
 --
-myWorkspaces    = ["1:code","2:web","3:msg","4:office","5:media","6:vm","7","8","9"]
+myWorkspaces    = ["1:code","2:web","3:msg","4:office","5","6","7","8","9"]
  
 -- Border colors for unfocused and focused windows, respectively.
 --
@@ -144,20 +151,11 @@ myLayout = onWorkspace "2:web" (avoidStruts $ tabbed shrinkText myTabTheme ) $
            onWorkspace "4:office" (avoidStruts $ tabbed shrinkText myTabTheme||| spiral (6/7) ) $ 
            onWorkspace "3:msg" (avoidStruts $ spiral (5/6) ) $ 
            onWorkspace "9" (avoidStruts $ tabbed shrinkText myTabTheme) $ 
-
            avoidStruts $ tiled ||| Mirror tiled ||| tabbed shrinkText myTabTheme ||| Full ||| spiral (6/7) {-||| noBorders (fullscreenFull Full)-} 
-
   where
-     -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
- 
-     -- The default number of windows in the master pane
      nmaster = 1
- 
-     -- Default proportion of screen occupied by master pane
      ratio   = 1/2
- 
-     -- Percent of screen to increment by when resizing panes
      delta   = 3/100
  
 ------------------------------------------------------------------------
@@ -189,8 +187,8 @@ myManageHook = (composeAll . concat $
             ,[ className =? c --> doShift "4:office" | c <- myOffice ]
             ,[ resource  =? c --> doShift "4:office" | c <- myOffice ]
             ,[ prefixTitle "libreoffice" <||> prefixTitle "LibreOffice" --> doShift "4:office" ]
-            ,[ className =? c --> doShift "5:media" | c <- myMedia ]
-            ,[ className =? c --> doShift "6:vm" | c <- myVMs ]
+            ,[ className =? c --> doShift "5" | c <- myMedia ]
+            ,[ className =? c --> doShift "6" | c <- myVMs ]
             ])
     where
     prefixTitle prefix = fmap (prefix `isPrefixOf`) title
@@ -198,10 +196,10 @@ myManageHook = (composeAll . concat $
     myIgnores = [""]
     myResourceIgnores =  ["desktop_window", "Do", "kdesktop","mailterm"]
 
-    myCode = ["Gnome-terminal","gnome-panel","Gedit", "Pgadmin3"]
+    myCode = ["Gnome-terminal","gnome-panel","Gedit", "Pgadmin3", "mate-terminal", "Mate-terminal"]
     myBrowsers = ["Firefox", "Google-chrome"]
     myComms = ["Pidgin","Thunderbird"]
-    myOffice = ["OpenOffice.org 3.2","libreoffice-calc","libreoffice-writer","libreoffice-startcenter","LibreOffice 3.3","LibreOffice 3.4","soffice","Evince"]
+    myOffice = ["OpenOffice.org 3.2","libreoffice-calc","libreoffice-writer","libreoffice-startcenter","LibreOffice 3.3","LibreOffice 3.4","soffice","Evince","mysql-workbench-bin"]
     myMedia = ["Rhythmbox","vlc"]
     myVMs = ["VirtualBox"]
  
@@ -232,48 +230,48 @@ myFocusFollowsMouse = True
 myStartupHook = return ()
 
 ------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
- 
--- Run xmonad with the settings you specify. No need to modify this.
---
-main = do
-	{-xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar"-}
-	xmonad 
-    $ ewmh 
-    $ defaults {
-		logHook        = updatePointer (Relative 0.5 0.5)
-        --dynamicLogWithPP $ xmobarPP {
-          --                      ppOutput = hPutStrLn xmproc
-            --                    , ppTitle = xmobarColor "#FFB6B0" "" . shorten 100
-              --                  , ppCurrent = xmobarColor "#CEFFAC" ""
-                --                , ppSep = "   "
-                  --              }
-	 	, manageHook = manageDocks <+> myManageHook <+> namedScratchpadManageHook scratchpads
+{-xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar"-}
+{-topStatusBar <- spawnPipe myTopStatusBar-}
+  {-spawnPipe myTopStatusBar-}
+  {-spawnPipe "conky -c ~/.xmonad/conky/dzen | dzen2 -p -xs 1 -ta l -e 'onstart=lower'"-}
+  {-h <- spawnPipe "dzen2 -p -xs 1 -ta l -fn Verdana-12 -e 'onstart=lower'"-}
+  {-xmproc <- spawnPipe "dzen2 -p -xs 1 -ta l -fn Verdana-12 -e 'onstart=lower'"-}
+  {-xmproc <- spawnPipe "xmobar /home/myszka/.xmonad/xmobar.config"-}
+  {-spawnPipe "/home/myszka/.xmonad/statusbar.sh"-}
+  --xmonad $ ewmh $ (defaults {
+    {-logHook            =  updatePointer (Relative 0.5 0.5),-}
+    {-logHook            =  updatePointer (0.5, 0.5) (0, 0),-}
+    {-logHook            = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn h  } <+> updatePointer (Relative 0.5 0.5),-}
+    {-logHook            = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn h  },-}
+    {-logHook            = dynamicLogWithPP dzenPP-}
+    {-logHook            = dynamicLogWithPP xmobarPP-}
+                              {-{ ppOutput = hPutStrLn xmproc , ppTitle = xmobarColor "green" "" . shorten 50 },-}
+------------------------------------------------------------------------
 
-		, startupHook = setWMName "LG3D"
-	}
+main :: IO ()
+main = do
+  dzenLeftBar <- spawnPipe myXmonadBar
+  dzenRightBar <- spawnPipe myStatusBar
+  xmonad $ withUrgencyHookC dzenUrgencyHook { args = ["-bg", "red", "fg", "black", "-xs", "1", "-y", "25"] } urgencyConfig { remindWhen = Every 15 } $  (defaults {
+    logHook = myLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd, 
+    manageHook         = manageDocks <+> myManageHook <+> namedScratchpadManageHook scratchpads,
+    startupHook        = setWMName "LG3D"
+  }
     `additionalKeys` [ ((myModMask .|. controlMask, xK_l     ), spawn "gnome-screensaver-command  --lock")
-        -- scratchpads
         , ((mod4Mask, xK_h), namedScratchpadAction scratchpads "htop")
         , ((mod4Mask, xK_n), namedScratchpadAction scratchpads "notes")
         , ((mod4Mask, xK_t), namedScratchpadAction scratchpads "term")
         , ((mod4Mask, xK_d), namedScratchpadAction scratchpads "mytodo")
         , ((mod4Mask, xK_w), namedScratchpadAction scratchpads "worktodo")
-        , ((mod4Mask .|. controlMask, xK_x), shellPrompt defaultXPConfig)
-        , ((mod4Mask .|. controlMask, xK_n), appendFilePrompt defaultXPConfig "/home/jmyszka/xmonad.notes")
         , ((mod4Mask, xK_r), rotSlavesUp)
-        -- launch dmenu
-        , ((myModMask,               xK_p     ), spawn "exe=`dmenu_path | ~/bin/dmenu` && eval \"exec $exe\"")
-    ]
+        , ((mod4Mask, xK_p ), spawn "gnome-do")
+        , ((mod4Mask .|. controlMask, xK_x), shellPrompt defaultXPConfig)
+        , ((mod4Mask .|. controlMask, xK_n), appendFilePrompt defaultXPConfig "/home/myszka/xmonad.notes")
+        , ((mod4Mask .|. controlMask, xK_t), sendMessage ToggleStruts)
+        , ((mod4Mask .|. controlMask, xK_p), spawn "dmenu_run")
+    ])
 
--- A structure containing your configuration settings, overriding
--- fields in the default config. Any you don't override, will 
--- use the defaults defined in xmonad/XMonad/Config.hs
--- 
--- No need to modify this.
---
 defaults = defaultConfig {
-      -- simple stuff
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
         borderWidth        = myBorderWidth,
@@ -281,13 +279,35 @@ defaults = defaultConfig {
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
- 
-      -- key bindings
-        {-keys               = myKeys,-}
         mouseBindings      = myMouseBindings,
- 
-      -- hooks, layouts
         layoutHook         = smartBorders $ myLayout,
         manageHook         = myManageHook ,
         startupHook        = myStartupHook
     }
+
+myXmonadBar = "dzen2 -xs '0' -y '0' -h '14' -w '1440' -ta 'l' -fg '#FFFFFF' -bg '#1B1D1E'"
+myStatusBar = "conky -c /home/myszka/.conkyrc | dzen2 -xs '2' -w '1440' -h '14' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'"
+myBitmapsDir = "/home/myszka/.xmonad/dzen2"
+
+myLogHook :: Handle -> X ()
+myLogHook h = dynamicLogWithPP $ defaultPP
+    {
+      ppCurrent           =   dzenColor "#ebac54" "#1B1D1E" . pad
+        , ppVisible           =   dzenColor "white" "#1B1D1E" . pad
+        , ppHidden            =   dzenColor "white" "#1B1D1E" . pad
+        , ppHiddenNoWindows   =   dzenColor "#7b7b7b" "#1B1D1E" . pad
+        , ppUrgent            =   dzenColor "black" "red" . pad
+        , ppWsSep             =   " "
+        , ppSep               =   "  |  "
+        , ppLayout            =   dzenColor "#ebac54" "#1B1D1E" .
+                                    (\x -> case x of
+                                     "ResizableTall"             ->      "^i(" ++ myBitmapsDir ++ "/tall.xbm)"
+                                     "Mirror ResizableTall"      ->      "^i(" ++ myBitmapsDir ++ "/mtall.xbm)"
+                                     "Full"                      ->      "^i(" ++ myBitmapsDir ++ "/full.xbm)"
+                                     "Simple Float"              ->      "~"
+                                     _                           ->      x
+                                    )
+        , ppTitle             =   (" " ++) . dzenColor "white" "#1B1D1E" . dzenEscape
+        , ppOutput            =   hPutStrLn h
+    }
+
